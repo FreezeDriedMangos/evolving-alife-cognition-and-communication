8ad616dc44b699ae35f632713e71348aa89ec68c
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-12-28 10:23:21 -0500

    feat: support for GUI, walls changing over time, simulations lasting longer as evolution continues, among other improvements and bugfixes

--- Evolution.py
+++ Evolution.py
@@ -1,6 +1,3 @@
-"""
-2-input XOR example -- this is most likely the simplest possible example.
-"""
 
 from __future__ import print_function
 import os
@@ -8,19 +5,45 @@ import neat
 import visualize
 import Agent
 import World
+import GUI
+
+WALL_CHANGE_SPEED = 0.5
+GEN_TIMER_CHANGE_SPEED = 0.25
 
 grandparents = []
 parents = []
 cur_gen = []
+generation_number = 0
 
 p = None
 world = None
+
+
+# TODO: collision detection
+# TODO: raycasting (sight)
+# TODO: food (that adds to max_age)
+# TODO: a gui to show what's happening (toggleable to run faster/slower)
+# TODO: seed the RNG
+
+# Food location randomization:
+# use the random walk algorithm from AI for games (have a walk direction and randomly add/subtract a small amount from it)
+# grow the distance walked based on the generation number
+# if the walk direction points outside of the world area, reflect the portion of the vector that's outside the bounds off the wall
+# 
+# eventual improvement: instead of directly using the NEAT nn, use it to generate 4 other NNs
+#   - a convolutional one to process vision
+#   - (recurrent) one to take the convolutional output and sensory info as input and output "state" (an N length vector)
+#   - (recurrent) one to take state from the previous frame and output reward (a single float from -1 to 1)
+#   - (recurrent) one to take state as input and output action - this one will also be tuned using RL
+#          (should this one also handle graph operations, should I have another precedesor to handle graph ops, or should I just toss graph ops?)
+
 
 def eval_genomes(genomes, config):
     # init
     global grandparents
     global parents
     global cur_gen
+    global generation_number
 
     grandparents = parents
     parents = cur_gen
@@ -29,14 +52,17 @@ def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
         genome.fitness = 0
         net = neat.nn.recurrent.RecurrentNetwork.create(genome, config)
-        agent = Agent.Agent(net, *p.reproduction.ancestors[genome_id])
+        agent = Agent.Agent(net, *[parent[2] for parent in parents if parent[0] in p.reproduction.ancestors[genome_id]])
 
         cur_gen.append((genome_id, genome, agent))
 
+    world.update_walls(int((generation_number*WALL_CHANGE_SPEED)**2))
+
     # simulate
     world.init_agents([agent for (genome_id, genome, agent) in grandparents+parents+cur_gen]) # skips agents that are dead
-    for i in range(10000): # simulate 10,000 'seconds'
+    for i in range(min(10000, 100 + int((generation_number*GEN_TIMER_CHANGE_SPEED)**2))): # simulate 10,000 'seconds'
         world.iteration()
+        GUI.draw(world)
 
     # rate performance
     for ((genome_id, genome, agent), index) in zip(cur_gen, range(len(cur_gen))):
@@ -46,6 +72,8 @@ def eval_genomes(genomes, config):
 
         # TODO: try (num_agents_in_species_still_alive / num_agents_in_species)
         # TODO: try (agent.max_age - agent.age) * (num_agents_in_species_still_alive / num_agents_in_species)
+
+    generation_number += 1
 
 
 def run(config_file):
@@ -90,6 +118,8 @@ def run(config_file):
 
 
 if __name__ == '__main__':
+    GUI.init()
+
     print("Num inputs: " + str(Agent.TOTAL_INPUT_COUNT))
     print("Num outputs: " + str(Agent.TOTAL_OUTPUT_COUNT))
     # Determine path to configuration file. This path manipulation is
