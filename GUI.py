ea5732834d0c89a6c5d91c95c936815145331577
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-12-28 10:21:46 -0500

    feat: created a GUI

---
+++ GUI.py
@@ -0,0 +1,62 @@
+
+import pygame
+import sys
+import math
+
+
+# draw each creature
+# draw each soundwave, kinda like this:
+# -=%#
+#   -=%#
+#    -=%#
+#     -=%#
+#     -=%#
+#    -=%#
+#   -=%#
+# -=%#
+
+
+
+
+
+screen = None
+screen_width = 500
+screen_height = 500
+scale = 10
+def init():
+	global screen
+
+	pygame.init()
+	# pygame.display.list_modes()
+
+	# Set up the drawing window
+	screen = pygame.display.set_mode([500, 500])
+
+def draw(world):
+	screen.fill((255, 255, 255))
+
+	for agent in world.agents:
+		pygame.draw.circle(screen, (100, 100, 100), (agent.x*scale, agent.y*scale), 1*scale)
+
+	for sound_wave in world.sound_waves:
+		volume = int(sound_wave.volume_at_distance(sound_wave.radius))
+		color = (0,0,0)
+		if volume == 0:
+			volume = 1
+			color = (50, 50, 50)
+		pygame.draw.circle(screen, color, (sound_wave.origin_x*scale, sound_wave.origin_y*scale), sound_wave.radius*scale, width=volume)
+
+	for y in range(0, math.ceil(world.world_max_y/world.wall_size)):
+		for x in range(0, math.ceil(world.world_max_x/world.wall_size)):
+			if world.walls[x][y]:
+				pygame.draw.rect(screen, (0, 0, 0), (world.wall_size*x*scale, world.wall_size*y*scale, world.wall_size*scale, world.wall_size*scale))
+			# color = tuple(int(255*(world.walls[x][y])%1) for i in range(3))
+			# pygame.draw.rect(screen, color, (world.wall_size*x*scale, world.wall_size*y*scale, world.wall_size*scale, world.wall_size*scale))
+
+	# Flip the display
+	pygame.display.flip()
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit(0)
