8ece0ba88078c2365cc537a026b5ff2a2fad3fa8
FreezeDriedMangoes <claytonharting+github@gmail.com>
2021-12-31 14:42:05 -0500

    feat: extending optboid to include obstacle avoidance

---
+++ clay_pygameboid.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+"""
+A simple pyglet engine to do some 2D rendering.
+Used to display boids at given positions
+"""
+from __future__ import division, print_function, absolute_import, unicode_literals
+
+import pygame
+from math import *
+import optboid_extension
+
+class DummyObject:
+	pass
+
+def rotate_vector(v, a):
+	p = DummyObject()
+	p.x = v[0]
+	p.y = v[1]
+
+	return (p.x * cos(a) - p.y * sin(a), p.x * sin(a) + p.y * cos(a))
+
+class World(object):
+	"""
+	world class with draw functions for entities
+	"""
+	
+	screen = None
+	screen_width = 700
+	screen_height = 700
+	scale = 10
+
+	boid_template = [(0, 1), (0.5, -1), (-0.5, -1)]
+
+	def __init__(self, swarm, offx, offy):
+		self.swarm = swarm
+		self.ents = swarm.boids  # this will point to a list of boids
+		self.ent_size = 15.0
+		self.num_ents = len(swarm.boids)
+		# self.fps = clock.ClockDisplay()
+		self.o_x = offx
+		self.o_y = offy
+
+		pygame.init()
+		self.screen = pygame.display.set_mode([self.screen_width, self.screen_height])
+
+	def draw_entity(self, e):
+		""" Draws a boid """
+		scale = self.ent_size
+		scaled_boid_points = [(p[0] * scale, p[1] * scale) for p in self.boid_template]
+		rotated_boid_points = [rotate_vector(p, e.rotation-pi/2) for p in scaled_boid_points]
+		boid_points = [(p[0] + e.position.x + self.o_x, p[1] + e.position.y + self.o_y) for p in rotated_boid_points]
+
+		color = (0, 0, 0) if not isinstance(e, optboid_extension.BoidObstacle) else (100, 0, 0)
+		pygame.draw.polygon(self.screen, color, boid_points)
+
+
+	# def draw_grid(self):
+	#     cw = self.swarm.cell_width
+	#     w = cw * self.swarm.divisions
+	#     for i in range(self.swarm.divisions):
+	#         xy = i*cw
+	#         glLoadIdentity()
+	#         glBegin(GL_LINES)
+	#         glColor4f(0.5, 0.5, 0.5, 0)
+	#         glVertex2f(0, xy)
+	#         glVertex2f(w, xy)
+	#         glEnd()
+
+	#         glBegin(GL_LINES)
+	#         glColor4f(0.5, 0.5, 0.5, 0)
+	#         glVertex2f(xy, 0)
+	#         glVertex2f(xy, w)
+	#         glEnd()
+
+	def draw(self):
+		self.screen.fill((255, 255, 255))
+		
+		# self.fps.draw()
+
+		#self.draw_grid()
+		for ent in self.ents:
+			self.draw_entity(ent)
+
+sim = optboid_extension.FlockAndObstacleSimulation(150, 750)
+world = World(sim.swarm, -25, -25)
+
+# window = pyglet.window.Window(700, 700, vsync=False)
+
+
+# @window.event
+# def on_draw():
+#     window.clear()
+#     world.draw()
+
+
+# def update(dt):
+# 	sim.update(dt)
+# 	world.draw()
+
+
+# def idle(dt):
+# 	pass
+
+# clock.schedule(update)
+# clock.schedule(idle)
+
+# if __name__ == '__main__':
+#     pyglet.app.run()
+
+if __name__ == '__main__':
+	clock = pygame.time.Clock()
+	
+	crashed = False
+
+	while not crashed:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				crashed = True
+
+		pygame.display.update()
+		dt = clock.tick(30)
+		
+		sim.update(dt/400)
+		world.draw()
---
+++ optboid_extension.py
@@ -0,0 +1,129 @@
+from __future__ import division, print_function, absolute_import, unicode_literals
+import libraries.optboid.simulation as optboid
+
+
+import math
+import operator
+import types
+from math import atan2, sin, cos, floor, ceil
+import random
+import collections
+
+class BoidObstacle(optboid.Boid):
+	def update(self, t):
+		pass
+
+class SmartBoid(optboid.Boid):
+	
+	_avoid_f = optboid.Vector2(0, 0)
+	_nearest_obstacle_distance = float('inf')
+
+	def interact(self, actors):
+		"""
+		Unit-unit interaction method, combining a separation force, and velocity
+		alignment force, and a cohesion force.
+
+		Many examples separate these into different functions for clarity
+		but combining them means we need fewer loops over the neibor list
+		"""
+
+		self._sep_f.clear()
+		self._align_f.clear()
+		self._cohes_sum.clear()
+		self._avoid_f.clear()
+		self._nearest_obstacle_distance = float('inf')
+
+		obstacle_count = 0
+		count = 0
+		self.neighbors = len(actors)
+
+		for other in actors:
+			# vector pointing from neighbors to self
+			diff = self.position - other.position
+			d = abs(diff)
+
+			# Only perform on "neighbor" actors, i.e. ones closer than arbitrary
+			# dist or if the distance is not 0 (you are yourself)
+			if 0 < d < self.influence_range:
+
+				diff.normalize()
+				if d < self.minsep:
+#                    diff *= self.max_force
+#                else:
+					diff /= d  # Weight by distance
+
+				if not isinstance(other, BoidObstacle):
+					count += 1
+					self._sep_f += diff
+
+					self._cohes_sum += other.position  # Add position
+
+					# Align - add the velocity of the neighbouring actors, then average
+					self._align_f += other.velocity
+				elif isinstance(other, BoidObstacle):
+					obstacle_count += 1
+					distance = (self.position - other.position).magnitude()
+
+					if distance < self._nearest_obstacle_distance:
+						self._nearest_obstacle_distance = distance
+
+						ahead = self.position + self.velocity
+						# self._avoid_f = ahead - other.position
+						self._avoid_f += other.position
+
+
+		if count > 0:
+			# calc the average of the separation vector
+			# self._sep_f /=count don't div by count if normalizing anyway!
+			self._sep_f.normalize()
+			self._sep_f *= self.max_speed
+			self._sep_f -= self.velocity
+			optboid.limit(self._sep_f, self.max_force)
+
+			# calc the average direction (normed avg velocity)
+			# self._align_f /= count
+			self._align_f.normalize()
+			self._align_f *= self.max_speed
+			self._align_f -= self.velocity
+			optboid.limit(self._align_f, self.max_force)
+
+			# calc the average position and calc steering vector towards it
+			self._cohes_sum /= count
+			cohesion_f = self.steer(self._cohes_sum, True)
+
+			self._sep_f *= self.sep_strength
+			self._align_f *= self.align_strength
+			cohesion_f *= self.cohesion_strength
+
+			# calc the avoidance force
+			self._avoid_f /= obstacle_count if obstacle_count > 0 else 1
+			self._avoid_f = self.position + (self._sep_f + cohesion_f + self._align_f) - other.position
+			self._avoid_f.normalize()
+			self._avoid_f *= self.max_speed
+			self._avoid_f -= self.velocity
+			optboid.limit(self._avoid_f, self.max_force*2)
+
+			# finally add the velocities
+			sum = self._sep_f + cohesion_f + self._align_f + self._avoid_f
+
+			self.acceleration = sum
+
+
+class FlockAndObstacleSimulation(optboid.FlockSimulation):
+	def __init__(self, starting_units=100, field_size=800, starting_obstacles=50):
+		"""
+		"""
+		self.swarm = optboid.BoidSwarm(field_size+2*40, optboid.Boid.influence_range+5)  # /2
+		self.field_size = field_size
+		self.pad = 40  # use to keep boids inside the play field
+
+		for i in range(starting_units):
+			b = SmartBoid(random.uniform(100, 400),
+						random.uniform(100, 400))
+			self.swarm.boids.append(b)
+		for i in range(starting_obstacles):
+			b = BoidObstacle(random.uniform(100, 400),
+							random.uniform(100, 400))
+			self.swarm.boids.append(b)
+		self.swarm.rebuild()
+		self._cumltime = 0  # calculation var
